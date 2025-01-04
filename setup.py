e2ce0f0b9e3bb960b01d02ade9f7e7126496ca7b
piotr-kubica <piotrekk8@gmail.com>
2025-01-04 18:22:08 +0100

    Bump version to 0.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mac-intruder",
-    version="0.0.1",
+    version="0.1",
     description="A Python script to detect new devices on a local network and send notifications via email.",
     author="Piotr Kubica",
     author_email="",
